bf9cb5f4e6f81dafee2006a267e38c9cd1567b90
MauRomano <maurromano@gmail.com>
2024-12-30 17:12:52 -0300

    Added distance matrix, transpose graph and strongly connected components

--- main.py
+++ main.py
@@ -13,10 +13,43 @@ class Vertice:
         self.tempoFinal = None
 
 class Grafo:
-    def __init__(self, s, vertList):
+    def __init__(self, vertList, s=0):
         self.verticeInicial = s
         self.vertices = vertList
         self.tempo = 0
+
+    def sortVerticesBy(self, param, rev = False):
+        if param == 0:
+            # Do not sort
+            sortVertices = self.vertices
+        elif param == 1:
+            #Sort by id
+            sortVertices = sorted(self.vertices, key=lambda x: x.id, reverse=rev)
+        elif param == 2:
+            #Sort by Tempo Final
+            print("Sorting by tempo final")
+            sortVertices = sorted(self.vertices, key=lambda x: x.tempoFinal, reverse=rev)
+
+        self.vertices = sortVertices
+
+
+    def showGrafo(self):
+        for v in self.vertices:
+            strAdj = " ".join([str(x) for x in v.adj])
+            print(f"{v.id}: {strAdj}")
+        pass
+
+    def returnVerticeById(self, id):
+        v = self.vertices[0] #If id not found return first vertices
+        for vl in self.vertices:
+            if(vl.id == id):
+                v = vl
+                break
+        return v
+
+
+    def returnMatrix(self):
+        pass
 
     def resetCor(self):
         for v in self.vertices:
@@ -28,8 +61,11 @@ class Grafo:
             v.tempoInicial = None
             v.tempoFinal = None
 
-    def bfs(self):
-        vi = self.vertices[grafo.verticeInicial]
+    def bfs(self, idVerticeInical = None):
+        if idVerticeInical is None:
+            vi = self.vertices[grafo.verticeInicial]
+        else:
+            vi = self.vertices[idVerticeInical]
         vi.d = 0
         vi.cor = Vertice.CINZA
 
@@ -47,36 +83,76 @@ class Grafo:
         dList = [v.d for v in self.vertices]
         return dList
 
+    def matrizDeDistancias(self):
+        distanceMatrix = {}
+        for v in self.vertices:
+            self.resetCor()
+            distanceMatrix[v.id] = self.bfs(v.id)
+        return distanceMatrix
+
     def visitaDFS(self, u):
         self.tempo = self.tempo + 1
         u.tempoInicial = self.tempo
         u.cor = Vertice.CINZA
-        for v in [self.vertices[x] for x in u.adj]:
+        print(f"{u.id}: {u.tempoInicial}")
+        print(u.adj)
+        for v in [self.returnVerticeById(x) for x in u.adj]:
+            print(u.id, v.id)
+            print(v.adj)
             if v.cor == Vertice.BRANCO:
                 self.visitaDFS(v)
         self.tempo = self.tempo + 1
         u.tempoFinal = self.tempo
+        print(f"{u.id}: {u.tempoFinal}")
         u.cor = Vertice.PRETO
-
 
     def dfs(self):
         self.resetTempo()
         self.resetCor()
+        ol = [x.id for x in self.vertices]
+        print(ol)
         for u in self.vertices:
             if u.cor == Vertice.BRANCO:
+                print(u.id)
                 self.visitaDFS(u)
         tiList = [v.tempoInicial for v in self.vertices] 
         tfList = [v.tempoFinal for v in self.vertices] 
         return tiList, tfList
 
+    def parentesisExpression(self):
+        parentesisList = ["" for _ in range(2*len(self.vertices)+1)]
+        for v in self.vertices:
+            parentesisList[v.tempoInicial] = f"({v.id}"
+            parentesisList[v.tempoFinal] = f"{v.id})"
+        return " ".join(parentesisList)
+
+    def transposeGrafo(self):
+        transposeDict = {k:[] for (k,_) in zip([v.id for v in self.vertices], range(len(self.vertices)))}
+        verticeListTransposto = [Vertice(v.id) for v in self.vertices]
+        grafoTransposto = Grafo(verticeListTransposto)
+        for v in self.vertices:
+            for vAdj in v.adj:
+                grafoTransposto.vertices[vAdj].adj.append(v.id)
+        return grafoTransposto
+
+    def fortementeConectados(self):
+        _, lf = grafo.dfs()
+        grafoT = grafo.transposeGrafo()
+        for v in grafoT.vertices:
+            v.tempoFinal = lf[v.id]
+        grafoT.verticeInicial = grafoT.vertices[0].id
+        grafoT.sortVerticesBy(2, True)
+        grafoT.dfs()
+        return grafoT.parentesisExpression()
+
+
 if __name__ == '__main__':
     ### Inicialização
     n, m = (int(tmp) for tmp in input().split(" "))
     vertList = [Vertice(id) for id in range(n)]
-    grafo = Grafo(0, vertList)
+    grafo = Grafo(vertList)
     for k in range(m):
         i, j = (int(tmp) for tmp in input().split(" "))
         grafo.vertices[i].adj.append(j)
-    tiList, tfList = grafo.dfs()
-    print(tiList)
-    print(tfList)
+    print(grafo.fortementeConectados())
+
